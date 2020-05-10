b6721bf8614ac1d32ac6c898debdc086b9e7762e
Douglas Dennys <douglasdennys@yahoo.com>
2020-05-10 16:09:42 -0300

    subindo shell script pra configurar mongodb

---
+++ init.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo "Update and Upgrade Ubuntu 18.04"
+apt update && apt upgrade -y  
+
+echo "Install MongoDB"
+apt autoremove -y && apt-get install gnupg && wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && apt-get update && apt-get install -y mongodb-org  
+
+echo "Disabled Upgrade MongoDB"
+echo "mongodb-org hold" | sudo dpkg --set-selections && echo "mongodb-org-server hold" | sudo dpkg --set-selections && echo "mongodb-org-shell hold" | sudo dpkg --set-selections && echo "mongodb-org-mongos hold" | sudo dpkg --set-selections && echo "mongodb-org-tools hold" | sudo dpkg --set-selections 
+
+echo "Enabled and Started MongoDB"
+systemctl enable mongod.service && systemctl start mongod.service
+
+echo "Move /etc/sysctl.conf"
+mv ./lib/sysctl.conf /etc/sysctl.conf
+
+echo "Move /lib/systemd/system/mongod.service"
+mv ./lib/mongod.service /lib/systemd/system/mongod.service
+
+echo "Move /etc/security/limits.conf"
+mv ./lib/limits.conf /etc/security/limits.conf
+
+echo "Reload Service"
+systemctl daemon-reload
+
+echo "Create Disable THP Service"
+mv ./lib/disable-thp.service /etc/systemd/system/disable-thp.service
+
+echo "Enabled and Started Disable THP Service"
+systemctl enable disable-thp.service && systemctl start disable-thp.service
+
+mongo <<EOF
+use admin 
+db.createUser({ user: "root", pwd: "123", roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] })
+exit
+EOF
+
+echo "Move Configuration MongoDB"
+mv ./lib/mongod.conf /etc/mongod.conf
+
+echo "Reload Service MongoDB"
+systemctl restart mongod.service
